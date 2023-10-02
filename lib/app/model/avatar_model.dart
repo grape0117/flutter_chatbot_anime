3ac4e1a09a2e309cedd2068b8177a09b1999127b
Masaki <trojan0412@outlook.com>
2023-10-02 13:02:48 +0800

    pubsec_change

---
+++ lib/app/model/avatar_model.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class AvatarModel {
+  Color borderColor;
+  double borderWidth;
+
+  AvatarModel({this.borderColor = Colors.transparent, this.borderWidth = 1.0});
+}
